ec3e4a7454bb429e237dbe9d3e6fd4c7a9687253
Venicius Alves <venicius.alves@gmail.com>
2019-11-14 18:57:53 -0300

    first commit

---
+++ js/init.js
@@ -0,0 +1,22 @@
+(function ($) {
+
+
+  $(function () {
+    $('.nav a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+
+      $('html, body').animate({
+        scrollTop: targetOffset - 100
+      }, 500);
+    });
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
